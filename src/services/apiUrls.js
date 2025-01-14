9498e3c67c33e8f22a16840ae73dc44c5dcb010d
vishal kumar shaw <vishalkumar.msit@gmail.com>
2025-01-14 18:33:20 +0530

    api link updated

--- src/services/apiUrls.js
+++ src/services/apiUrls.js
@@ -1,2 +1,3 @@
 // export const BASE_API_URL = "http://localhost:5000/api/";
-export const BASE_API_URL = "http://192.168.31.126:5000/api/";
+// export const BASE_API_URL = "http://192.168.31.126:5000/api/";
+export const BASE_API_URL = "https://disease-apis.onrender.com/api/";
